a84d40267ef6c7804de9f057db4022c13425d164
Nandini2510 <nandini976804@gmail.com>
2024-08-02 00:18:10 -0400

    Drift detection modified

--- detect_drift.sh
+++ detect_drift.sh
@@ -3,9 +3,13 @@
 CONFIG=$1
 SNS_TOPIC_ARN=${SNS_TOPIC_ARN}
 
+# Change to the correct directory
+cd "${WORKSPACE}/${CONFIG}"
+
 PLAN_OUTPUT=$(terraform plan -var="alert_email=ynandini0625@gmail.com" -detailed-exitcode 2>&1)
 EXITCODE=$?
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
 
 publish_metric() {
     aws cloudwatch put-metric-data \
