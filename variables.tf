03a7983b547a93f848ecef91f86917c70c5a5ae3
Nandini2510 <nandini976804@gmail.com>
2024-08-19 00:15:14 -0400

    Resolved provider conflict

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-2"
-}
-
 variable "alert_email" {
   description = "Email address for drift alerts"
   type        = string
